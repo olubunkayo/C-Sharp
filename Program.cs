941bcaf694ca7af244cc683a44a5888686c9161b
olubunkayo <tolu.olufotebi@gmail.com>
2020-03-18 22:21:07 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string personName = ""; 
+
+            Console.WriteLine("Please enter your name: ");
+            
+            personName = Console.ReadLine(); 
+
+            Console.WriteLine("Hello" + personName);
+          
+        }
+    }
+}
